966ed300e8c059edff6cebf2bc5ae8a65759fcd2
Luke <lgeorgeson17@heathfieldcc.co.uk>
2023-08-09 18:22:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from kivy.app import App
-
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.uix.button import Button
-
 from kivy.uix.textinput import TextInput
-
-
 class MainApp(App):
-
     def build(self):
         self.operators = ["/", "*", "+", "-"]
         self.last_was_operator = None
@@ -20,11 +14,8 @@ class MainApp(App):
         main_layout.add_widget(self.solution)
         buttons = [
             ["7", "8", "9", "/"],
-
             ["4", "5", "6", "*"],
-
             ["1", "2", "3", "-"],
-
             [".", "0", "C", "+"],
         ]
         for row in buttons:
@@ -36,16 +27,10 @@ class MainApp(App):
                 button.bind(on_press=self.on_button_press)
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
-
-
         equals_button = Button(
-
             text="=", pos_hint={"center_x": 0.5, "center_y": 0.5}
-
         )
-
         equals_button.bind(on_press=self.on_solution)
-
         main_layout.add_widget(equals_button)
         return main_layout
 
